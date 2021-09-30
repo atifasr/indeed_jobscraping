08a9d19af6f739e39eb957a72b055ce6a59b40fd
atif shafi <atifshafi63@gmail.com>
2021-09-30 10:10:49 +0530

    last modified

--- lead_gen.py
+++ lead_gen.py
@@ -1,7 +1,9 @@
 
+from concurrent import futures
 import requests
 from bs4 import BeautifulSoup
 from requests.api import request
+from concurrent.futures import ThreadPoolExecutor
 
 #for setting connection and getting soup obj
 def get_content(page_no=0):
@@ -42,20 +44,45 @@ def get_links(content):
     for links in job_links:
         job_link_list.append(links['href']) 
     get_job_detail(job_link_list[0])
-    
+
+
+
+ 
+def modify_salary(salary_str):
+    print(salary_str)
+    salary={
+        
+    }
+    salary_start,salary_end = salary_str.rstrip(" a month").rstrip(" a year").split("-")
+    salary['salary_start']=salary_start
+    salary['salary_end']=salary_end
+    return salary
+ 
+       
     
 
 
 #getting job data dictionary
 
 def get_data(soup):
-    content = soup.find_all("table",class_ = "jobCard_mainContent")
-    job_details_list = []
+    job_links = soup.find_all("a",class_ = "tapItem")
 
-    for val in content:
+    content = soup.find_all("table",class_ = "jobCard_mainContent")
+    job_descrip = soup.find_all("table",class_ ="jobCardShelfContainer")
+    job_details_list = []
+    for val,descrip,links in zip(content,job_descrip,job_links):
+        semi_descript = descrip.find("div",class_ = "job-snippet")
+        date_ = descrip.find("span",class_ = "date")
+
         result_content  = val.find_all("td",class_ = "resultContent")
-        job_descrip = val.find_all("table",class_ = "jobCardShelfContainer")
         for result in result_content:
+            try:
+                descrip = semi_descript.find("ul").get_text()
+            except:
+                descrip = None
+            job_date = date_.get_text()
+
+
             job_title = result.find("h2",class_= "jobTitle").get_text()
             company_name = result.find("span",class_ = "companyName").get_text()
             company_location = result.find("div",class_ = "companyLocation").get_text()
@@ -63,21 +90,28 @@ def get_data(soup):
                 'job_title':job_title,
                 'company_name':company_name,
                 'company_location':company_location,  
+                'job_descrip':descrip,
+                'job_posted_date':job_date,
+                'job_link':f"https://in.indeed.com{links['href']}"
             }
             try:
                 salary = result.find("span", class_ = "salary-snippet").get_text()
-                job_detail['salary'] = salary
+                job_detail['salary'] = modify_salary(salary)
             except Exception as e:
                 salary = 0
             
             job_details_list.append(job_detail)
 
-    print(job_details_list)
+    return job_details_list
 
 
 
 
 content  = get_content()
-get_links(content)
-#get_data(content)
+# get_links(content)
+executor = ThreadPoolExecutor(max_workers=3)
+future = executor.submit(get_data,content)
+job_details = future.result()
 
+
+print(job_details)
